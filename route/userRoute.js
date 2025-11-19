797fb86ed884f628de2a0b71f221eeb3b724803e
HelloAyan <helloayan171@gmail.com>
2025-11-20 00:45:05 +0600

    fix: JWT token issue fixed

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { register, login } from '../controller/userController.js';
+import { register, login, getAllUsers } from '../controller/userController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 
 const route = express.Router();
 
 route.post('/register', register);
 route.post('/login', login);
+route.get('/users', verifyToken, getAllUsers)
 
 export default route;
