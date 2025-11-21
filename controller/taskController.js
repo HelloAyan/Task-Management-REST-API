eeace87eef851544525379aba5f3a0378094ff27
HelloAyan <helloayan171@gmail.com>
2025-11-22 01:31:04 +0600

    feat: task model added

---
+++ controller/taskController.js
@@ -0,0 +1,9 @@
+import Tasks from "../model/taskModel.js"
+
+export const createTask = async (req, res) => {
+    try {
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error" };)
+    }
+}
